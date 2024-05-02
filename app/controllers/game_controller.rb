b629150311fe6c2a1544cf43834713c2ab76983d
drdpatel <165212603+drdpatel@users.noreply.github.com>
2024-05-02 01:02:11 +0000

    done?

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -18,5 +18,34 @@ class GameController < ApplicationController
     def get_home
       render({ :template => "paths/home" })
     end
+
+    def play_scissors
+      moves = ["rock", "paper", "scissors"]
+
+      @comp_move = moves.sample
     
+      if @comp_move == "scissors"
+        @outcome = "tied"
+      elsif @comp_move == "rock"
+        @outcome = "lost"
+      else @comp_move == "paper"
+        @outcome = "won"  
+      end
+    
+    end
+
+    def play_paper
+      moves = ["rock", "paper", "scissors"]
+
+      @comp_move = moves.sample
+    
+      if @comp_move == "paper"
+        @outcome = "tied"
+      elsif @comp_move == "scissors"
+        @outcome = "lost"
+      else @comp_move == "rock"
+        @outcome = "won"  
+      end
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   get("/home", { :controller => "pages",:action =>"home" })
   get("/", { :controller => "game", :action =>"get_home" })
   get("/rock", { :controller => "game", :action =>"play_rock"})
+  get("/paper", { :controller => "game", :action =>"play_paper"})
+  get("/scissors", { :controller => "game", :action =>"play_scissors"})
 
 end
